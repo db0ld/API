e92d46f3b621482b7fad793a66ece95a79e21fb2
Guillaume Louvigny <tuxkowo@gmail.com>
2013-04-01 21:23:13 +0200

    API - Achievements - Pagination

--- configurations/app.js
+++ configurations/app.js
@@ -3,5 +3,7 @@ module.exports = {
 	'version': 1,
 	'api_path': '/api/',
 	'db_path': 'mongodb://localhost/life',
-	'public_path': 'public'
+	'public_path': 'public',
+	'def_offset': 0,
+	'def_limit': 5
 };
--- controllers/achievements.js
+++ controllers/achievements.js
@@ -1,26 +1,41 @@
 var api_utils = require('../utils/api.js');
+var api_params = require('../config.js');
 
 module.exports = function(app, models) {
+    // get all achievements
     app.get(api_utils.makePath('achievement'), function (req, res) {
-          return models.Achievement.find(function (err, items) {
-            return api_utils.apiResponse(res, req, err || items);
+          return models.Achievement.find()
+            .limit(req.query.limit || api_params.def_limit)
+            .skip(req.query.offset || api_params.def_offset)
+            .exec(function (err, items) {
+              return api_utils.apiResponse(res, req, err || items);
           });
     });
 
+    // get a single achievement
     app.get(api_utils.makePath('achievement/:id'), function (req, res) {
           return models.Achievement.findById(req.params.id, function (err, item) {
             return api_utils.apiResponse(res, req, err || item);
           });
     });
 
+    // add an achievement
     app.post(api_utils.makePath('achievement'), function (req, res) {
-      var item;
+      var item = new models.Achievement(api_utils.requestToObject(req, models.Achievement));
 
-      console.log(req.body);
-
-      item = new models.Achievement(api_utils.requestToObject(models.Achievement));
       item.save(function (err) {
         return api_utils.apiResponse(res, req, err || item);
+      });
+    });
+
+    // Update achievement
+    app.put(api_utils.makePath('achievement/:id'), function (req, res){
+      return models.Achievement.findById(req.params.id, function (err, item) {
+        item = api_utils.requestToObject(req, models.Achievement, item);
+
+        return item.save(function (err) {
+          return api_utils.apiResponse(res, req, err || item);
+        });
       });
     });
 };
