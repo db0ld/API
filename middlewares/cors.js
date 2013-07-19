c719cf9cd71bd3328172116c0071472499b0cfc7
db0company <db0company@gmail.com>
2013-07-19 06:14:58 +0200

    Intercept OPTIONS requests in CORS middleware so they don't fail with a 404 error.

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,12 +2,16 @@
 //CORS middleware
 var allowCrossDomain = function(domain) {
     return function(req, res, next) {
-    
+
 	res.header('Access-Control-Allow-Origin', domain);
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 	res.header('Access-Control-Allow-Headers', 'Content-Type');
-	
-	next();
+
+	if (req.method == 'OPTIONS') {
+	    res.send(200);
+	} else {
+    	    next();
+	}
     }
 }
 
